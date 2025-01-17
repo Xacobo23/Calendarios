bd99345807d4cd8eb89440aa36757886521ee612
Xulio <a22julioaa@iessanclemente.net>
2025-01-17 10:54:13 +0100

    implement(templates): implementar homepage con Django

--- codigo/django/appHorario/settings.py
+++ codigo/django/appHorario/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'session',
     #'admin',
     'core',
-    'bootstrap5',
+    #'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
