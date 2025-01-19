9403db9990cbbd016f6bf6b695e78a42ba00078c
xulio <a22julioaa@iessanclemente.net>
2025-01-19 01:18:01 +0100

    feature(styles): mejorados estilos del header

--- codigo/django/core/views.py
+++ codigo/django/core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import user_passes_test
 
 def homepage (request):
-    return render(request, 'homepage.html')
+    return render(request, 'homepage.html', {'title': 'Inicio'})
