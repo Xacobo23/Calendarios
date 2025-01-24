f20743257ce3d197284a38daf86581ab384ab680
A22DavidCR <A22DavidCR@iessanclemente.net>
2025-01-24 11:53:25 +0100

    feature(modules): base para añadir módulos

--- codigo/django/appHorario/settings.py
+++ codigo/django/appHorario/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     #'bootstrap5',
     "student",
     "user",
-    "slippers",
 ]
 
 MIDDLEWARE = [
--- codigo/django/module/urls.py
+++ codigo/django/module/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from . import views
 
-# Aquí se establecen las rutas a las que va a poder acceder el usuario. El primer parámetro es la ruta en si, el segundo es 
+# Aquí se establecen las rutas a las que va a poder acceder el usuario. El primer parámetro es la ruta en si, el segundo es
 # la vista que se va a cargar (ver archivo views.py) y el tercero es el nombre de esa página en concreto. Este último
 # parámetro es importante porque luego se usa para redirigir las páginas con ese nombre.
 urlpatterns = [
-    # path('add/', views.add_module, name='add_module'),  
-    path('list/', views.module_list, name='module_list'),  
+    # path('add/', views.add_module, name='add_module'),
+    path("list/", views.module_list, name="module_list"),
+    path("add/", views.module_add, name="module_add"),
     # path('nombre-vista/', views.ejemplo_vista, name='ejemplo')
 ]
