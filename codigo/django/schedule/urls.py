92c165b829a7b63f21c66028ebade75df202963e
Xulio <a22julioaa@iessanclemente.net>
2025-01-31 12:57:55 +0100

    feature(cosas)

--- codigo/django/schedule/urls.py
+++ codigo/django/schedule/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('view-schedule/', views.view_schedule, name='view_schedule'),  
+    path('select-schedule/', views.select_schedule, name='select_schedule'),
+    path('view-schedule/<int:fp_id>', views.view_schedule, name='view_schedule'),  
 ]
