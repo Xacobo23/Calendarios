fc4506b4c6360408896234856b536327f753628d
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2025-02-04 16:36:45 +0100

    Modules - Teacher relation

--- codigo/django/module/admin.py
+++ codigo/django/module/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
+from .models import Module, Enrolled
+from teacher.models import TeacherModule
 
-from .models import Module
 
+# Inline para TeacherModule (desde la perspectiva de Module)
+class TeacherModuleInline(
+    admin.TabularInline
+):  # También puedes usar admin.StackedInline
+    model = TeacherModule
+    extra = (
+        1  # Número de formularios vacíos que se muestran para añadir nuevas relaciones
+    )
+
+
+# Admin para Module
 class ModuleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'fp')
-    list_filter = ('code', 'fp')
-    search_fields = ('name', )
+    list_display = ("code", "name", "fp", "course")  # Campos a mostrar en la lista
+    inlines = [TeacherModuleInline]  # Añade el inline para TeacherModule
 
+
+# Registrar los modelos en el admin
 admin.site.register(Module, ModuleAdmin)
-
+admin.site.register(Enrolled)
