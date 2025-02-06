1cbc552068652736e2134f76a4a54dd8714d72c9
a22julioaa <a22julioaa@iessanclemente.net>
2025-02-06 10:13:53 +0100

    Redirecciones cambiadas

--- codigo/django/core/views.py
+++ codigo/django/core/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render, redirect
 from django.conf.urls import handler404
 
 def homepage (request):
-    return redirect('student_list')
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return redirect('student_list')
+        else:
+            return redirect('my_schedules')
+    else:
+        return redirect('login')
 
